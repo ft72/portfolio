586702a75116ae9f0707c4bca48f58e81cf7a29f
Mercurius Inc. <mercuriusdevelops@gmail.com>
2024-06-23 22:38:19 +0500

    Uploading portfolio

---
+++ js/commands.js
@@ -0,0 +1,103 @@
+var password = "sheesh";
+var linkedin = "https://www.linkedin.com/in/fa1zantahir/";
+var instagram = "https://www.instagram.com/fa1zantahir/";
+var linktree = "https://linktr.ee/faizan.dev/";
+var email = 'mailto:faizan@mercurius-inc.com';
+var resume = "https://drive.usercontent.google.com/download?id=1fM2BD_CeUhGsPaqdHHvOp6qR6icR3l83&export=download";
+var video = 'mailto:faizan@mercurius-inc.com';
+var rickroll = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+var ecom = 'https://ecommerce-faizan.mercurius-inc.com';
+var skillswap = 'https://skillswap.mercurius-inc.com';
+var garlicsoss = 'https://garlicsoss.com';
+
+
+faizan = [
+  "<br>",
+  "Hello World!",
+  "My name is — you guessed it — M Faizan Tahir.",
+  "I'm a web developer & UI/UX Designer and a Software Engineering student.",
+  "I am attending Comsats University Islamabad. I'll be graduating in Q1, 2025.",
+  "I design and build websites, web apps and mobile applications.",
+  "Just like this one. I have 9 months of experience in web developement and designing.",
+  "If you are looking for someone to build a website for your business or indivual needs,",
+  "you know how to contact me. 😉",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "You are an individual seeking understanding, purpose, and connection in the vast and complex tapestry of human existence.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/fa1zantahir' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/fa1zantahir' + '</a>',
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on linktree, or confidential.",
+  '<br>',
+  '<a href="' + linktree + '" target="_blank">Linktree</a>            Linktree for all the projects.',
+  '<br>',
+  '<a href="' + skillswap + '" target="_blank">SkillSwap</a>           SkillSwap is a freelance marketplace that streamlines your freelancing journey.',
+  '<br>',
+  '<a href="' + ecom + '" target="_blank">Ecommerce</a>           This is an E-Commerce website developed using Laravel. It can be used for almost all kind of products.',
+  '<br>',
+  '<a href="' + garlicsoss + '" target="_blank">Garlic Soss</a>         Garlic Soss is a portfolio website of a graphic and visual designer.',
+  '<br>',
+  '<a href="' + rickroll + '" target="_blank">DO NOT CLICK HERE</a>   A very confidential project, Coming soon.',
+  '<br>',
+  'Dont worry, I\'ll upload all my projects soon. Anyways, here\'s a video of cat dancing.',
+  '<iframe width="560" height="315" src="https://www.youtube.com/embed/E3RcD6LleYU?autoplay=1;control=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">faizan</span>         Who is Faizan?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">resume</span>         View and download my resume?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           I think you\'ve seen what this one does.',
+  '<span class="command">email</span>          Reach out via email',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Faizan Tahir(FT) Not A Corporation. No rights reserved.</span>',
+  '<br>',
+  '         _____________  _____________  _____________  _____________       _____________   ______________ ',
+  '        /   _________/ /   _____    / /____    ____/ /_______     /      /   _____    /  /   ______    /',
+  '       /   /          /   /    /   /      /   /             /   /       /   /    /   /  /   /     /   /',
+  '      /   /______    /   /____/   /      /   /            /   /        /   /____/   /  /   /     /   /',
+  '     /   _______/   /   _____    /      /   /           /   /         /   _____    /  /   /     /   /',
+  '    /   /          /   /    /   / _____/   /____      /   /________  /   /    /   /  /   /     /   /',
+  '   /___/          /___/    /___/ /_____________/    /_____________/ /___/    /___/  /___/     /___/',
+  '<br>',
+  '<span class="color2">Howdy, this is my personal portfolio website.</span>',
+  "<span class=\"color2\">To view a list of available commands, simply type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+  "<span class=\"inherit\">NOTE: Write commands with no spaces.</span>",
+
+
+];
+
+resume = [
+  "<br>",
+  'Resume       <a href="' + resume + '" target="_blank">Download' + "</a>",
+  "<br>",
+  '<iframe src="https://drive.google.com/file/d/1fM2BD_CeUhGsPaqdHHvOp6qR6icR3l83/preview" width="640" height="480" allow="autoplay"></iframe>'
+];
